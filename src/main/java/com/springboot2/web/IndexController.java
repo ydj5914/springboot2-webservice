3c106d61761ab0df44b24433d363e2af0121d9b1
ydj5914 <!Qeownd6590>
2020-01-22 16:32:58 +0900

    commitTest

---
+++ src/main/java/com/springboot2/web/IndexController.java
@@ -0,0 +1,18 @@
+package com.springboot2.web;
+
+import com.springboot2.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
